695ed645c49524f3f00b655f1715d47889d7eb7a
Dênis Azevedo <denis.viegas.azevedo@gmail.com>
2020-08-17 19:29:52 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Worlds'});
+app.get('/projects', (require, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (require, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (require, response) => {
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (require, response) => {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3',
+  ]);
 });
 
 app.listen(3333, () => {
